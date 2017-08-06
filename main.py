684ccb5aba9b6fa8c668242050cc579ce103d8d7
Justus Karlsson <Justus Karlsson>
2017-08-06 05:23:42 +0200

    second

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def stream_handler(message):
     print(message["path"]) # /-K7yGTTEp7O549EzTYtI
     print(message["data"][1]) # {'title': 'Pyrebase', "body": "etc..."}
 
-my_stream = db.child("computers/1/images").stream(stream_handler)
+my_stream = db.child("computers").stream(stream_handler)
