258b524ac8c78b246478425b1a3b26bf5d7e8ad3
Justus Karlsson <Justus Karlsson>
2017-08-06 05:34:10 +0200

    bla

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ db = firebase.database()
 def stream_handler(message):
     print(message["event"]) # put
     print(message["path"]) # /-K7yGTTEp7O549EzTYtI
-    print(message["data"][1]) # {'title': 'Pyrebase', "body": "etc..."}
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
 
-my_stream = db.child("computers").stream(stream_handler)
+my_stream = db.child("spots").stream(stream_handler)
