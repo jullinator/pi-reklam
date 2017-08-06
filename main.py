3f9e681bbba98dd554335f997ff26e88dab102a8
Justus Karlsson <Justus Karlsson>
2017-08-06 05:54:39 +0200

    as

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from firebase import firebase
+import gui
 
 db = firebase.database()
+storage = firebase.storage()
 spots = db.child("spots")
 
 
 
 all_spots = spots.order_by_child("computer").equals_to("computer1").get()
 
-for spot in all_spots.each():
-    print(spot.val())
+for spotChild in all_spots.each():
+    spot = spotChild.val()
+    storage.child("images/"+spot["image"]).download("images/"+spot["image"])
+    #gui.make_image(spot["image"], spot["image"])
 
+#gui.show_image()
 
 
 #def stream_handler(message):
